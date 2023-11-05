ba1bb93714615d9dc56290a0852bea96fce7235b
Federico Brigante <me@fregante.com>
2023-11-05 13:30:31 +0800

    Restore rules for JS files

--- index.js
+++ index.js
@@ -269,6 +269,10 @@ const config = {
 			files: ["**/*.js"],
 			rules: {
 				"@typescript-eslint/no-unsafe-argument": "off",
+				"@typescript-eslint/no-unsafe-assignment": "off",
+				"@typescript-eslint/no-unsafe-call": "off",
+				"@typescript-eslint/no-unsafe-member-access": "off",
+				"@typescript-eslint/no-unsafe-return": "off",
 			},
 		},
 		{
