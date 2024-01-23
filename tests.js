e39a281ba0862273a1fab8378b1072514662d449
Federico Brigante <me@fregante.com>
2024-01-23 19:42:49 +0800

    Disable "unsafe-*" rules in test files

--- tests.js
+++ tests.js
@@ -15,6 +15,10 @@ module.exports = {
 		// Loosen types a bit to facilitate testing
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/no-unsafe-argument": "off",
+		"@typescript-eslint/no-unsafe-member-access": "off",
+		"@typescript-eslint/no-unsafe-call": "off",
+		"@typescript-eslint/no-unsafe-assignment": "off",
+		"@typescript-eslint/no-unsafe-return": "off",
 		"@typescript-eslint/no-non-null-assertion": "off",
 
 		// Tests can import any file
