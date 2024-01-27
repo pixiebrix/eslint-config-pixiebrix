c04b5bd7d3dff4d72095193a91d6e17afbdcd9d1
Federico Brigante <me@fregante.com>
2024-01-27 20:49:00 +0800

    Let's see if it works

--- fixtures.ts
+++ fixtures.ts
@@ -3,6 +3,5 @@
  * Each line MUST be preceded by `eslint-disable-next-line`, unless it's the "valid" version
  */
 
-// eslint-disable-next-line no-restricted-imports
 import { useSyncExternalStore } from "use-sync-external-store";
 import { useSyncExternalStore } from "use-sync-external-store/shim"; /* Valid */
