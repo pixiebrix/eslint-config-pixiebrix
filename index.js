ca76a70a8bd6d71f98f18e2228811cc4a3a4d418
Federico Brigante <me@fregante.com>
2024-02-26 00:41:36 +0800

    Revert changes to `no-restricted-imports` for now

--- index.js
+++ index.js
@@ -53,9 +53,68 @@ const config = {
 			},
 		],
 
-		"no-restricted-imports": ["error", require("./no-restricted-imports")],
-
 		"no-restricted-syntax": ["error", ...require("./no-restricted-syntax")],
+
+		"no-restricted-imports": [
+			"error",
+			{
+				// Documentation: https://eslint.org/docs/rules/no-restricted-imports#options
+				paths: [
+					{
+						name: "lodash",
+						importNames: ["lowerCase"],
+						message: "Use the native String.toLowerCase method instead.",
+					},
+				],
+				patterns: [
+					{
+						group: ["*/__mocks__/*"],
+						message:
+							"Mocks should not be imported directly, they’re automatically picked up where needed.",
+					},
+					{
+						group: ["lodash/*"],
+						message: 'You can import "lodash" instead of "lodash/*".',
+					},
+					{
+						group: ["immer/*"],
+						importNames: ["WritableDraft"],
+						message: 'Use this instead: import { type Draft } from "immer"',
+					},
+					{
+						group: ["react-spinners"],
+						message:
+							"Use the local <Loader/> component instead, it's already centered.",
+					},
+					{
+						group: ["react-bootstrap/*", "!react-bootstrap/types"],
+						message:
+							'You can import "react-bootstrap" instead of "react-bootstrap/*".',
+					},
+					{
+						group: ["../*"],
+						message:
+							'Use root-based imports (`import "@/something"`) instead of relative imports.',
+					},
+					{
+						group: ["formik"],
+						importNames: ["Form", "Formik"],
+						message: 'Use this instead: import "@/components/form/Form"',
+					},
+					{
+						group: ["react-bootstrap"],
+						importNames: ["Form"],
+						message: 'Use this instead: import "@/components/form/Form"',
+					},
+					{
+						group: ["webext-detect-page"],
+						importNames: ["isDevToolsPage"],
+						message:
+							'Use this instead: import { isPageEditor } from "@/utils/expectContext";',
+					},
+				],
+			},
+		],
 
 		// Avoid imports with side effects
 		"import/no-unassigned-import": [
