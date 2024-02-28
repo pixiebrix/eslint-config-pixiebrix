1b0ecb7aff83667c3041737b65151d6514eb9c06
Federico Brigante <me@fregante.com>
2024-02-29 00:54:50 +0800

    Restrict imports of classnames to cx (#232)

--- no-restricted-syntax.test.ts
+++ no-restricted-syntax.test.ts
@@ -1,9 +1,14 @@
 /** @file File that verifies rules in `no-restricted-syntax`. Each line MUST be preceded by `eslint-disable-next-line`, which acts as an "expect" assertion */
 
 /* Unrelated issues */
+/* eslint-disable import/no-duplicates */
 /* eslint-disable unicorn/no-useless-undefined */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+// eslint-disable-next-line no-restricted-syntax
+import classnames from "classnames";
+import cx from "classnames";
 
 // eslint-disable-next-line no-restricted-syntax
 export const id = crypto.randomUUID();
